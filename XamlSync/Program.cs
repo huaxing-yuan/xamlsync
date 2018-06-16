7bbeecdc5d83da9149f2ee8339eba2818da54006
Huaxing YUAN <newlucky@gmail.com>
2018-06-17 00:25:40 +0200

    rewrite to keep source file order and comments.

--- XamlSync/Program.cs
+++ XamlSync/Program.cs
@@ -16,22 +16,108 @@ namespace XamlSync
         {
             string sourceFile = args[0];
             FileInfo fi = new FileInfo(sourceFile);
+            XmlDocument xmlDoc = new XmlDocument();
+            List<KeyValuePair<string, string>> keyvalues = new List<KeyValuePair<string, string>>();
 
-            ResourceDictionary resourceDictionary;
             using (FileStream fs = new FileStream(sourceFile, FileMode.Open, FileAccess.Read))
             {
-                XamlReader reader = new XamlReader();
-                resourceDictionary = (ResourceDictionary)reader.LoadAsync(fs);
+                xmlDoc.Load(fs);
+                int nodeIndex = 0;
+                var nodes = xmlDoc.LastChild.ChildNodes;
+               
+                foreach(XmlNode node in nodes)
+                {
+                    nodeIndex++;
+                    if(node.LocalName == "String")
+                    {
+                        string value = node.InnerText;
+                        string key = node.Attributes[0].Value;
+                        keyvalues.Add(new KeyValuePair<string, string>(key, value));
+                    }
+                    else if(node.LocalName == "#comment")
+                    {
+                        keyvalues.Add(new KeyValuePair<string, string>("#comment", node.Value));
+                    }
+                }
+
             }
 
             foreach (var file in fi.Directory.GetFiles("*.xaml"))
             {
                 if (file.Name != fi.Name)
                 {
-                    Merge(resourceDictionary, file);
+                    Merge(keyvalues, file);
                 }
             }
 
+        }
+
+        private static void Merge(List<KeyValuePair<string, string>> keyvalues, FileInfo file)
+        {
+
+            XmlDocument xmlDoc = new XmlDocument();
+            using (FileStream fs = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+            {
+                xmlDoc.Load(fs);
+                XmlNamespaceManager xmlNamespaceManager = new XmlNamespaceManager(xmlDoc.NameTable);
+                xmlNamespaceManager.AddNamespace("s", "clr-namespace:System;assembly=mscorlib");
+                xmlNamespaceManager.AddNamespace("x", "http://schemas.microsoft.com/winfx/2006/xaml");
+                XmlNode previousNode = null;
+                foreach (var keyvalue in keyvalues)
+                {
+                    if (keyvalue.Key != "#comment")
+                    {
+                        var node = xmlDoc.SelectSingleNode($"//s:String[@x:Key='{keyvalue.Key}']", xmlNamespaceManager);
+                        if (node == null)
+                        {
+                            var newNode = xmlDoc.CreateElement("s:String", "clr-namespace:System;assembly=mscorlib");
+                            var attr = xmlDoc.CreateAttribute("x", "Key", "http://schemas.microsoft.com/winfx/2006/xaml");
+                            attr.Value = keyvalue.Key;
+                            newNode.Attributes.Append(attr);
+                            newNode.InnerText = keyvalue.Value;
+                            if (previousNode != null)
+                            {
+                                xmlDoc.LastChild.InsertAfter(newNode, previousNode);
+                            }
+                            else
+                            {
+                                xmlDoc.LastChild.AppendChild(newNode);
+                            }
+                            previousNode = newNode;
+                        }
+                        else
+                        {
+                            previousNode = node;
+                        }
+                    }
+                    else
+                    {
+                        var node = xmlDoc.SelectSingleNode($"//comment()[. = '{keyvalue.Value}']");
+                        if (node == null)
+                        {
+                            var newNode = xmlDoc.CreateComment(keyvalue.Value);
+                            if (previousNode != null)
+                            {
+                                xmlDoc.LastChild.InsertAfter(newNode, previousNode);
+                            }
+                            else
+                            {
+                                xmlDoc.LastChild.AppendChild(newNode);
+                            }
+                            previousNode = newNode;
+                        }
+                    }
+                }
+            }
+
+            using (FileStream fs = new FileStream(file.FullName, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                var writer = XmlWriter.Create(fs, new XmlWriterSettings() { Indent = true, IndentChars = "    ", });
+                xmlDoc.Save(writer);
+                writer.Close();
+            }
+        
+               
         }
 
         private static void Merge(ResourceDictionary resourceDictionary, FileInfo file)

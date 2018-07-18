60cfeab0890bac55cdfd628cb485db14f952a180
Huaxing YUAN <newlucky@gmail.com>
2018-07-19 00:47:41 +0200

    use Azure translation api to translate automatically the missing strings to appropriate language.

--- XamlSync/Program.cs
+++ XamlSync/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,8 @@ namespace XamlSync
             FileInfo fi = new FileInfo(sourceFile);
             XmlDocument xmlDoc = new XmlDocument();
             List<KeyValuePair<string, string>> keyvalues = new List<KeyValuePair<string, string>>();
+
+
 
             using (FileStream fs = new FileStream(sourceFile, FileMode.Open, FileAccess.Read))
             {
@@ -52,9 +55,19 @@ namespace XamlSync
 
         }
 
+
+        static TranslatorService.TranslatorServiceClient client = new TranslatorService.TranslatorServiceClient(ConfigurationManager.AppSettings["API_KEY"]);
+
+        private static async Task<string> Translate(string originalString, string originalLang, string targetLang)
+        {
+            string result = await client.TranslateAsync(originalString, targetLang);
+            Console.WriteLine($"Translation: from {originalLang} : {originalString} -> to {targetLang} : {result} ");
+            return result;
+        }
+
         private static void Merge(List<KeyValuePair<string, string>> keyvalues, FileInfo file)
         {
-
+            Console.WriteLine("####### Checks file: " + file.Name);
             XmlDocument xmlDoc = new XmlDocument();
             using (FileStream fs = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
             {
@@ -74,7 +87,9 @@ namespace XamlSync
                             var attr = xmlDoc.CreateAttribute("x", "Key", "http://schemas.microsoft.com/winfx/2006/xaml");
                             attr.Value = keyvalue.Key;
                             newNode.Attributes.Append(attr);
-                            newNode.InnerText = keyvalue.Value;
+                            string to = file.Name.Substring(file.Name.IndexOf(".") + 1, file.Name.LastIndexOf(".") - file.Name.IndexOf(".") -1);
+                            string translation = Translate(keyvalue.Value, "en", to).Result;
+                            newNode.InnerText = translation;
                             if (previousNode != null)
                             {
                                 xmlDoc.LastChild.InsertAfter(newNode, previousNode);
